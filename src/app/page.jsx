2319fd1ade3d824e66000f3f6c2f49bf1d8c9368
Joslin-varsha <joslinvarsha55@gmail.com>
2026-09-21 11:48:52 +0530

    new-arrivals

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -17,9 +17,9 @@ export default function Home() {
       <Navbar />
       <Hero />
       <CategoryIcons />
+      <NewArrivals />
       <BestSelling />
       <Categories />
-      <NewArrivals />
       <BrandStory />
       <Testimonials />
       <Footer />
